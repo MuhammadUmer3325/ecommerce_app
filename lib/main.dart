8c978fd83bf2a5f1f3a49842a66aee3b7e95efc6
MUHAMMAD UMER <umerm3325@gmail.com>
2025-10-03 22:27:59 +0500

    Initial commit - add lib, assets, pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:laptop_harbor/core/theme/app_theme.dart';
 import 'screens/splash_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Firebase initialize
+void main() {
   runApp(const LaptopHarborApp());
 }
 
@@ -19,7 +16,7 @@ class LaptopHarborApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
 
-      // App start hone par sabse pehle SplashScreen chalegi
+      // Sabse pehle SplashScreen chalegi
       home: const SplashScreen(),
     );
   }
